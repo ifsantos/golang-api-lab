7bee7f2678f581f36ca6c8c93566b3969b0e5227
Ismael <fs.ismael@gmail.com>
2021-12-08 17:02:14 -0300

    Adding mock

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### Resources:
 - <https://gist.github.com/michaelkeevildown/9096cd3aac9029c4e6e05588448a8841?ref=morioh.com&utm_source=morioh.com>
 - <https://www.getcreditcardnumbers.com/>
+- <https://github.com/golang/mock>
---
+++ usecase/process_transaction/process_transaction_test.go
@@ -0,0 +1,44 @@
+package processtransaction
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/ifsantos/golang-api-lab/domain/entity"
+	"github.com/ifsantos/golang-api-lab/domain/repository/mock_repository"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessTransaction_ExecuteInvalidCreditCard(t *testing.T) {
+	input := TransactionDtoInput{
+		ID:                        "1",
+		AccountID:                 "1",
+		CreditCardNumber:          "123123132123123",
+		CreditCardName:            "Darth Vader",
+		CreditCardExpirationMonth: 12,
+		CreditCardExpirationYear:  time.Now().Year(),
+		CreditCardCVV:             123,
+		Amount:                    200,
+	}
+
+	expectedOutput := TransactionDtoOutput{
+		ID:           "1",
+		Status:       entity.REJECTED,
+		ErrorMessage: "invalid credit card number",
+	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repositoryMock := mock_repository.NewMockTransactionRepository(ctrl)
+	repositoryMock.EXPECT().
+		Insert(input.ID, input.AccountID, input.Amount, expectedOutput.Status, expectedOutput.ErrorMessage).
+		Return(nil)
+
+	usecase := NewProcessTransaction(repositoryMock)
+	output, err := usecase.Execute(input)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+
+}
