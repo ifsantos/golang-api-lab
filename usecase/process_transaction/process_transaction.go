3af1be109c71fb8818ffca8f079d0e3ba8b437e4
Ismael <fs.ismael@gmail.com>
2021-12-13 23:30:32 -0300

    process transaction

--- usecase/process_transaction/process_transaction.go
+++ usecase/process_transaction/process_transaction.go
@@ -1,1 +1,33 @@
 package processtransaction
+
+import (
+	"github.com/ifsantos/golang-api-lab/domain/entity"
+	"github.com/ifsantos/golang-api-lab/domain/repository"
+)
+
+type ProcessTransaction struct {
+	Repository repository.TransactionRepository
+}
+
+func NewProcessTransaction(repo repository.TransactionRepository) *ProcessTransaction {
+	return &ProcessTransaction{Repository: repo}
+}
+func (p *ProcessTransaction) Execute(input TransactionDtoInput) (TransactionDtoOutput, error) {
+	transaction := entity.NewTransaction()
+	transaction.ID = input.ID
+	transaction.AccountID = input.AccountID
+	transaction.Amount = input.Amount
+	_, invalidCC := entity.NewCreditCard(input.CreditCardNumber, input.CreditCardName, input.CreditCardExpirationMonth, input.CreditCardExpirationYear, input.CreditCardCVV)
+	if invalidCC != nil {
+		err := p.Repository.Insert(transaction.ID, transaction.AccountID, transaction.Amount, entity.REJECTED, invalidCC.Error())
+		if err != nil {
+			return TransactionDtoOutput{}, err
+		}
+		return TransactionDtoOutput{
+			ID:           transaction.ID,
+			Status:       entity.REJECTED,
+			ErrorMessage: invalidCC.Error(),
+		}, nil
+	}
+	return TransactionDtoOutput{}, nil
+}
