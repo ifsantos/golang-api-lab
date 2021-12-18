83fd47e9ea273ce93f75ceb30e7717009f050e69
Ismael <fs.ismael@gmail.com>
2021-12-17 21:39:24 -0300

    Adding Migrations

---
+++ adapter/repository/fixture/fixture.go
@@ -0,0 +1,28 @@
+package fixture
+
+import (
+	"context"
+	"database/sql"
+	"io/fs"
+	"log"
+
+	"github.com/maragudk/migrate"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Up(migrationsDir fs.FS) *sql.DB {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = migrate.Up(context.Background(), db, migrationsDir)
+	if err != nil {
+		return nil
+	}
+	return db
+}
+
+func Down(db *sql.DB, migrationsDir fs.FS) {
+	migrate.Down(context.Background(), db, migrationsDir)
+	db.Close()
+}
